5ee0428f8c40002b114207eab8bb47291d3f6cf0
carmencoloma <56000965+carmencoloma@users.noreply.github.com>
2019-11-18 15:20:31 -0800

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,33 @@
-// your script file for adding your own jquery
-$(function() {
-// Your Code from here on down. Don't delete that line above!
-  
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeOut(500);
+        }
+    })
+}
+
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);  
 
 
 
@@ -17,4 +43,4 @@ $(function() {
 
 
 // End of Your Code . Don't delete that line below!!
-});
+/*});*/
